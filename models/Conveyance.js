1adde62db48fc026d4b3a5389289830ccf0ab40d
unknown <ashikahmed121@gmail.com>
2023-08-27 10:31:47 +0600

    Conveyance model created

---
+++ models/Conveyance.js
@@ -0,0 +1,44 @@
+const { default: mongoose } = require("mongoose");
+
+
+const conveyanceSchema = mongoose.Schema(
+    {
+        employee: {
+            name: String,
+            email: String,
+            employeeId: {
+                type: ObjectId,
+                ref: "Employee",
+                required: [true, "Employee Id required"]
+            }
+        },
+        from: {
+            type: String,
+            required: [true, "From location is required"]
+        },
+        destination: {
+            type: String,
+            required: [true, "Destination location is required"]
+        },
+        date: {
+            type: Date,
+            required: [true, "Journey date is required"]
+        },
+        amount: {
+            type: Number,
+            required: [true, "Amount is required"]
+        },
+        purpose: {
+            type: String
+        },
+        partner: {
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Conveyance = mongoose.model("Conveyance", conveyanceSchema);
+module.exports = Conveyance;
