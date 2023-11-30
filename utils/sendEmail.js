dc457e7819ae95d0e4f2ae4c713e45bb9b5c4425
ashik-ahmed <ashikahmed121@gmail.com>
2023-11-30 22:46:07 +0600

    credentials added to env for email send

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -6,15 +6,17 @@ const transporter = nodemailer.createTransport({
     port: 587, // Use the appropriate port for your server
     secure: false, // Set to true for secure connections like TLS, false for non-secure
     auth: {
-        user: 'ashik@infotelebd.com',
-        pass: 'Itbl@it321',
+        user: process.env.AUTH_EMAIL,
+        pass: process.env.EMAIL_PASSWORD,
+        // user: 'office@infotelebd.com',
+        // pass: 'Itbl@it321',
     },
 });
 
 // Message object
 const message = {
-    from: 'ashik@infotelebd.com',
-    to: 'mushfiq@infotelebd.com',
+    from: 'obaydul@infotelebd.com',
+    to: 'ashik@infotelebd.com',
     subject: 'Hello from Node.js!',
     text: 'This is a test email sent from Node.js using Nodemailer and Zimbra.',
     html: '<p>This is a test email sent from <strong>Node.js</strong> using <em>Zimbra</em>.</p>',
@@ -23,7 +25,7 @@ const message = {
 // Send the email
 transporter.sendMail(message, (error, info) => {
     if (error) {
-        console.error('Error occurred:', error.message);
+        console.error('Error occurred:', error);
         return;
     }
     console.log('Email sent successfully!');
