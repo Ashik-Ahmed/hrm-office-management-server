e7fd6a658ad8cdfe77680884e85550ef4b31ec1f
ashik-ahmed <ashikahmed121@gmail.com>
2023-12-10 13:26:25 +0600

    a2p report sql updated

--- services/a2pReport.service.js
+++ services/a2pReport.service.js
@@ -10,8 +10,9 @@ exports.getPostgresDataService = async (date) => {
         port: 5432,
     });
 
-    const dbdata = await pool.query(`SELECT date, cli, client_id, operator,message_type, sum(count) Dipping_Count FROM public.dipping_summary where date='${date}' group by date, cli, client_id, operator, message_type order by 6 desc`);
-    // const dbdata = await pool.query(`select date,client_id,operator,message_type,count from public.dipping_summary where date='2023-12-03' and ans_business_code='1000' group by date, client_id,operator,message_type order by 1 desc, 5 desc;`);
+    // const dbdata = await pool.query(`SELECT date, cli, client_id, operator,message_type, sum(count) Dipping_Count FROM public.dipping_summary where date='${date}' group by date, cli, client_id, operator, message_type order by 6 desc`);
+    const dbdata = await pool.query(`select date, client_id, operator, message_type, sum(count) from public.dipping_summary where date='2023-07-20' group by date, client_id, operator,message_type order by 1 desc, 5 desc;`);
 
+    console.log(dbdata.rowCount);
     return dbdata;
 }
