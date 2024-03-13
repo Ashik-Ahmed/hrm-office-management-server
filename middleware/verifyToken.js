ea1e3fa8bdd65c741e924e6e4d129056f720d4ca
ashik-ahmed <ashikahmed121@gmail.com>
2024-03-13 22:22:44 +0600

    verifyToken middleware created

---
+++ middleware/verifyToken.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+
+
+
+module.exports = async (req, res, next) => {
+    try {
+        const token = req.headers?.authorization?.split(' ')?.[1];
+
+        if (!token) {
+            return res.status(401).json({
+                status: 'failed',
+                error: 'You are not logged in'
+            })
+        }
+
+        const decodecd = await promisify(jwt.verify)(token, process.env.ACCESS_TOKEN_SECRET);
+
+        req.user = decodecd;
+
+        next();
+
+    } catch (error) {
+        return res.status(403).json({
+            status: 'failed',
+            error: 'Invalid Token'
+        })
+    }
+}
