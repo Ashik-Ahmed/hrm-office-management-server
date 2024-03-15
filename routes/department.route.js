91b68196447e2e564916a7f431e3165e3ddc7910
ashik-ahmed <ashikahmed121@gmail.com>
2024-03-15 23:15:39 +0600

    token verification added on department route

--- routes/department.route.js
+++ routes/department.route.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const departmentController = require('../controllers/department.controller');
+const verifyToken = require('../middleware/verifyToken');
 
 const router = express.Router();
 
 router.route('/')
-    .get(departmentController.getAllDepartment)
-    .post(departmentController.createDepartment)
+    .get(verifyToken, departmentController.getAllDepartment)
+    .post(verifyToken, departmentController.createDepartment)
 
 router.route('/:id')
-    .patch(departmentController.updateDepartmentById)
+    .patch(verifyToken, departmentController.updateDepartmentById)
 
 // router.route('/:id')
 //     .delete()
