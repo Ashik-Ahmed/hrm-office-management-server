75e4090a7315c1426bbb2af8230dab6588854039
ashik-ahmed <ashikahmed121@gmail.com>
2024-03-16 23:03:41 +0600

    token verification added on all task routes

--- routes/task.route.js
+++ routes/task.route.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const taskController = require('../controllers/task.controller')
+const taskController = require('../controllers/task.controller');
+const verifyToken = require('../middleware/verifyToken');
 
 const router = express.Router()
 
 router.route('/get-all-task/:employeeEmail')
-    .get(taskController.getAllTasks)    //only get if the employee is an assignee and his own dept tasks. Management will get all dept. tasks
+    .get(verifyToken, taskController.getAllTasks)    //only get if the employee is an assignee and his own dept tasks. Management will get all dept. tasks
 
 router.route('/')
-    .post(taskController.createNewTask)
+    .post(verifyToken, taskController.createNewTask)
 // .get(taskController.getAllTasks)
 
 router.route('/:id')
-    .get(taskController.getTaskById)
-    .patch(taskController.updateTaskById)
+    .get(verifyToken, taskController.getTaskById)
+    .patch(verifyToken, taskController.updateTaskById)
 
 
 module.exports = router;
